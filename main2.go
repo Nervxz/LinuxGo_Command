1ff3b696d8431d51f22e07dc5c5a274e090c2eea
NervX <khangthai09@gmail.com>
2024-03-30 09:53:02 +0700

    commit

--- main2.go
+++ main2.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"os/exec"
 	"strings"
 )
 
@@ -26,14 +25,66 @@ func repl(in io.Reader, out io.Writer) {
 		if !scanned {
 			return
 		}
+
 		line := scanner.Text()
+		if line == "" {
+			continue
+		}
 		if err := inputCommand(line, out); err != nil {
 			fmt.Fprintln(out, err)
 		}
 	}
 }
 
-// ls,cd,pwd,etc...
+// ls,cd,pwd
+
+func inputCommand(line string, out io.Writer) error {
+
+	if line == "exit" || line == "quit" {
+		os.Exit(0)
+	}
+
+	args := strings.Fields(line)
+	switch args[0] {
+	case "ls":
+		return ls(out)
+	case "pwd":
+		return pwd(out)
+	case "cd":
+		return cd(args[1:])
+	default:
+		return fmt.Errorf("Please Reinput Command")
+	}
+}
+
+func ls(out io.Writer) error {
+	files, err := os.ReadDir(".")
+	if err != nil {
+		return err
+	}
+	for _, file := range files {
+		fmt.Fprintln(out, file.Name())
+	}
+	return nil
+}
+
+func pwd(out io.Writer) error {
+	pwd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(out, pwd)
+	return nil
+}
+
+func cd(args []string) error {
+	if len(args) < 1 {
+		return errors.New("cd: path")
+	}
+	return os.Chdir(args[0])
+}
+
+/*
 func inputCommand(line string, out io.Writer) error {
 
 	if line == "exit" || line == "quit" {
@@ -56,3 +107,4 @@ func inputCommand(line string, out io.Writer) error {
 		return cmd.Run()
 	}
 }
+*/
